3e2e2b974b5f3da8d401b5cda88baf23d175a4ac
manish <ms569624@gmail.com>
2024-04-28 20:35:04 +0530

    instiall commit

--- db/connect.js
+++ db/connect.js
@@ -2,9 +2,23 @@ const mongoose = require('mongoose');
 
 const uri = process.env.URI;
 
-const connectDB = () => {
-    console.log("connect Database");
-    return mongoose.connect(uri)
-}
+// const connectDB = () => {
+//     console.log("connect Database");
+//     return mongoose.connect(uri)
+// }
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${uri}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+};
+
 
 module.exports = connectDB;

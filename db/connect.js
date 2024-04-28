2219a3ca07916160891886562108f6629a58129c
manish <ms569624@gmail.com>
2024-04-28 22:23:23 +0530

    new changes

--- db/connect.js
+++ db/connect.js
@@ -2,23 +2,10 @@ const mongoose = require('mongoose');
 
 const uri = process.env.URI;
 
-// const connectDB = () => {
-//     console.log("connect Database");
-//     return mongoose.connect(uri)
-// }
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(`${uri}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
-};
+const connectDB = () => {
+    console.log("connect Database");
+    return mongoose.connect(uri)
+}
 
 
 module.exports = connectDB;

9d4288d2d87a4ca9cadddcb012ad5170aa331c14
manish <ms569624@gmail.com>
2024-05-02 00:07:36 +0530

    new chane

--- controllers/Category.js
+++ controllers/Category.js
@@ -171,6 +171,7 @@ const EditCategory = async (req, res) => {
         req.files.headinglogo[0].path.indexOf("\\images")
       );
     }
+    
     console.log(req.body)
     const items = req.body;
     const itemsdata = {

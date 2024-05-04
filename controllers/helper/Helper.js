270e46efb7128c383f0f0fdeb1f2067e62917274
manish <ms569624@gmail.com>
2024-05-05 01:51:32 +0530

    fixed logo in section

--- controllers/helper/Helper.js
+++ controllers/helper/Helper.js
@@ -2,47 +2,7 @@ const host = process.env.HOST;
 const port = process.env.PORT;
 const path = require("path");
 
-const ShubhkamnnaHelper = (req) => {
-    // const ReporterImage = `http://localhost:5000/image/${req.files.Image1[0].filename}`;
-    // const Image = `http://localhost:5000/image/${req.files.Image2[0].filename}`;
-    let ReporterImage, Image, Video, Audio;
 
-    if (req.files.Image1 && req.files.Image1.length > 0) {
-        ReporterImage = `/image/${req.files.Image1[0].filename}`;
-        console.log(req.files.Image1[0].filename);
-    } else {
-        // Handle the case where req.files.Image1 is not defined or empty
-    }
-
-    if (req.files.Image2 && req.files.Image2.length > 0) {
-        Image = `/image/${req.files.Image2[0].filename}`;
-    } else {
-        // Handle the case where req.files.Image2 is not defined or empty
-    }
-
-    if (req.files.Video && req.files.Video.length > 0) {
-        ReporterImage = `/image/${req.files.Image1[0].filename}`;
-        console.log(req.files.Video[0].filename);
-    } else {
-        // Handle the case where req.files.Image1 is not defined or empty
-    }
-    if (req.files.Video && req.files.Audio.length > 0) {
-        ReporterImage = `/image/${req.files.Image1[0].filename}`;
-        console.log(req.files.Audio[0].filename);
-    } else {
-        // Handle the case where req.files.Image1 is not defined or empty
-    }
-
-    const StateName = req.body.StateName;
-    const ReporterName = req.body.ReporterName;
-    const Heading = req.body.Heading;
-    const Matter = req.body.Matter;
-    const DatePlace = req.body.DatePlace;
-    const Subheading = req.body.Subheading;
-    const Designation = req.body.Designation;
-    // return { ReporterName, Heading, Matter, DatePlace, Subheading, Designation };
-    return { ReporterImage, ReporterName, Heading, Matter, DatePlace, Subheading, Designation, Image, Video, Audio };
-}
 const BlogHelper = (req) => {
 
     let Image, Video, Audio;
@@ -104,77 +64,7 @@ const BlogHelper = (req) => {
     // return { Category, Status, ReporterImage, ReporterName, Heading, Matter, DatePlace, Capton, Subheading, Designation, Image, Video, Audio };
 }
 
-const blockkHelper = (req) => {
 
-    let Image1, Image2;
-
-    if (req.files.Image1 && req.files.Image1.length > 0) {
-        Image1 = `/image/${req.files.Image1[0].filename}`;
-        // console.log(req.files.Image1[0].filename);
-    } else {
-        // Handle the case where req.files.Image1 is not defined or empty
-    }
-    if (req.files.Image2 && req.files.Image2.length > 0) {
-        Image2 = `/image/${req.files.Image2[0].filename}`;
-    } else {
-        // Handle the case where req.files.Image2 is not defined or empty
-    }
-
-    const name = req.body.name;
-    const Status = req.body.Status;
-    const background1 = req.body.background1;
-    const background2 = req.body.background2;
-    const SectionName = req.body.SectionName;
-    const SecondSection = req.body.SecondSection;
-
-    const data = {
-        Status,
-        background1,
-        background2,
-        SectionName,
-        SecondSection,
-        name,
-        Image1,
-        Image2,
-    };
-
-    return data;
-}
-
-const RajiyaHelper = (req) => {
-
-    let Image1, Image2;
-
-    if (req.files.Image1 && req.files.Image1.length > 0) {
-        Image1 = `/image/${req.files.Image1[0].filename}`;
-        // console.log(req.files.Image1[0].filename);
-    } else {
-        // Handle the case where req.files.Image1 is not defined or empty
-    }
-    if (req.files.Image2 && req.files.Image2.length > 0) {
-        Image2 = `/image/${req.files.Image2[0].filename}`;
-    } else {
-        // Handle the case where req.files.Image2 is not defined or empty
-    }
-    const Status = req.body.Status;
-    // const name = req.body.name;
-    const background1 = req.body.background1;
-    const background2 = req.body.background2;
-    const StateName = req.body.StateName;
-    const FirstLink = req.body.FirstLink;
-
-    const data = {
-        Status,
-        background1,
-        background2,
-        StateName,
-        FirstLink,
-        Image1,
-        Image2,
-    };
-
-    return data;
-}
 
 
 const ToplinkHelper = (req) => {
@@ -299,4 +189,4 @@ const AdvertHelper = (req) => {
 
 
 
-module.exports = { ShubhkamnnaHelper, BlogHelper, TeamHelper, FounderHelper, AdvertHelper, ToplinkHelper, blockkHelper, RajiyaHelper };
+module.exports = { BlogHelper, TeamHelper, FounderHelper, AdvertHelper, ToplinkHelper };
